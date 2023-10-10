eb78a26f42d6b3fbe16cad25b49b728847d32812
Victor Miralles <victor_miralles@hotmail.com>
2023-10-10 20:10:04 +0200

    feat: creamos método para guardar en CSV

--- balance/models.py
+++ balance/models.py
@@ -8,21 +8,22 @@ CLAVES_IGNORADAS = ["errores"]
 
 
 class Movimiento:
-    def __init__(self, fecha, concepto, tipo, cantidad):
+    def __init__(self, dic_datos):
 
         self.errores = []
+        fecha = dic_datos.get("fecha", "")
+        self.concepto = dic_datos.get("concepto", "Gastos varios")
+        self.tipo = dic_datos.get("tipo")
+        self.cantidad = dic_datos.get("cantidad")
         try:
             self.fecha = date.fromisoformat(fecha)
         except ValueError:
             self.fecha = None
             self.errores.append(f"****la fecha {fecha} no es una fecha válida")
 
-        self.concepto = concepto
-        self.tipo = tipo
-        self.cantidad = cantidad
-
       # property es un decorador qeu viene de serie con python
       # convierte el método en atributo por lo que al llamarla no hay que poner paréntesis
+
     @property
     def has_errors(self):
         return len(self.errores) > 0
@@ -43,12 +44,7 @@ class ListaMovimientos:
         with open(RUTA_FICHERO, "r") as fichero:
             reader = csv.DictReader(fichero)
             for fila in reader:
-                movimiento = Movimiento(
-                    fila["fecha"],
-                    fila["concepto"],
-                    fila["tipo"],
-                    fila["cantidad"]
-                )
+                movimiento = Movimiento(fila)
                 self.movimientos.append(movimiento)
 
     def agregar(self, movimiento):
@@ -70,21 +66,27 @@ class ListaMovimientos:
         4 recoger cada dato y guardarlo en una linea separada por comas
         - 
         """
-        with open(RUTA_FICHERO, "w") as fichero:  # con el open modo "w", al escribirlo ya lo vaciamos
+        with open(RUTA_FICHERO, "w") as csvfile:  # con el open modo "w", al escribirlo ya lo vaciamos
+            # convierte self.movimientos en un diccinario del que extramos las claves a una lista
             cabeceras = list(self.movimientos[0].__dict__.keys())
+            # lo que pasa  es que tb añade "errores" que como no queremos que figure en nuestra movimientos.csv, añademos
+            # a una CLAVES_IGNORADAS cuyo contenido extraeremos de cabeceras. asi es más mantenible.
             for clave in CLAVES_IGNORADAS:
                 cabeceras.remove(clave)
             writer = csv.DictWriter(csvfile, fieldnames=cabeceras)
             writer.writeheader()
 
-        for mov in self.movimientos:
-            mov_dict = mov.__dict__
-            for clave in CLAVES_IGNORADAS:
-                mov_dict.pop(clave)
-            writer.writerow(mov_dict)
+            for mov in self.movimientos:
+                mov_dict = mov.__dict__
+                for clave in CLAVES_IGNORADAS:
+                    mov_dict.pop(clave)
+                writer.writerow(mov_dict)
 
     def __str__(self):
         result = ""
         for mov in self.movimientos:
             result += f"\n{mov}"
         return result
+
+    def __repr__(self):
+        return self.__str__()
